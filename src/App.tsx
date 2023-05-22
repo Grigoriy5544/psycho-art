949951cbf226f55860e77912b114d521dbc3ee48
Grigoriy5544 <gregkotsenko@yandex.ru>
2023-05-22 21:35:16 +0400

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Sponsored from "./components/sponsored/Sponsored.tsx";
 import About from "./components/about/About.tsx";
 import Colleaction from "./components/colleaction/Colleaction.tsx";
 import FeaturedArtworks from "./components/featured-artworks/FeaturedArtworks.tsx";
+import Footer from "./components/footer/Footer.tsx";
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
             <About/>
             <Colleaction/>
             <FeaturedArtworks/>
+            <Footer/>
         </>
     )
 }
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,45 @@
+import styles from './Footer.module.scss';
+import youtube from "../../img/icons/youtube.svg"
+import discord from "../../img/icons/discord.svg"
+import instagram from "../../img/icons/instagram.png"
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <div className="container">
+                <div className={styles.groups}>
+                    <div className={styles.group}>
+                        <a href="/" className={styles.logo}>Psycho<span>Art</span></a>
+                        <p className={styles.text}>The best NFT marketplace website in the world and feel your
+                            experience in selling or buy our work</p>
+                    </div>
+                    <div className={styles.group}>
+                        <a className={styles.group__title}>About</a>
+                        <a className={styles.text}>Product</a>
+                        <a className={styles.text}>Resource</a>
+                        <a className={styles.text}>Term & Condition</a>
+                        <a className={styles.text}>FAQ</a>
+                    </div>
+                    <div className={styles.group}>
+                        <a className={styles.group__title}>Company</a>
+                        <a className={styles.text}>Our Team</a>
+                        <a className={styles.text}>Partner With Us</a>
+                        <a className={styles.text}>Privacy & Policy</a>
+                    </div>
+                    <div className={styles.group}>
+                        <a className={styles.group__title}>Contact</a>
+                        <a className={styles.text} href="tel:+0123456789">+012 3456789</a>
+                        <a className={styles.text} href="mailto:adorableprogrammer@gmail.com">adorableprogrammer@gmail.com</a>
+                        <div className={styles.socials}>
+                            <img src={youtube} alt=""/>
+                            <img src={discord} alt=""/>
+                            <img src={instagram} alt=""/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
